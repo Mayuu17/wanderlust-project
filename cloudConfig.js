70199b16db03da62abe170a57dd7db6226ca86a3
Mayuri Bobade <your-email@gmail.com>
2026-05-28 22:39:38 +0530

    fix: downgraded cloudinary for multer compatibility

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,6 +1,5 @@
-const cloudinary = require("cloudinary").v2;
-
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary"); 
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -10,7 +9,6 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-
   params: {
     folder: "wanderlust_DEV",
     allowedFormats: ["png", "jpg", "jpeg"],
