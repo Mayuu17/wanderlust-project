c82fbec737c399e43c64b1f9c90268ea362b8822
Mayuri Bobade <your-email@gmail.com>
2026-05-31 12:58:48 +0530

    Fix User model import

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,11 @@
+const User = require("../models/user");
+
 module.exports.renderSignupForm = (req, res) => {
   res.render("users/signup");
 };
 
-module.exports.signup = async (req, res) => {
+module.exports.signup = async (req, res, next) => {
+  
   try {
     let { username, email, password } = req.body;
     const newUser = new User({
